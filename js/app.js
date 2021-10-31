21589b55b032e84533bcdb9a3b3a396a5dbed781
ferhatkaymaz2023 <ferhatkaymaz2023@gmail.com>
2021-10-31 20:59:03 +0300

    Showtime fonksiyonunu yazdım

--- js/app.js
+++ js/app.js
@@ -5,5 +5,20 @@ const askGuestName = () =>
 }
 
 
+const showTime = () => 
+{
+    const date = new Date();    
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let y = date.getFullYear();
+
+
+    document.querySelector('#myClock').textContent = `${h}:${m}:${s} ${y}`;
+}
+
+
+
 askGuestName();
 
+setInterval(showTime,1000);
